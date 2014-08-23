b0ffbffee3292999ebf550f0c6e90e67e9942471
Michał Sidor (Michcioperz) <michcioperz@autistici.org>
2014-08-23 18:31:30 +0200

    now it should work (do not forget it's Python 3)

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,9 +1,33 @@
 from flask import Flask
-app = Flask(__name__)
+from peewee import *
+from flask_peewee.db import Database
+import os
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+DATABASE = {
+    'name': os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'fimichi.sqlite'),
+    'engine': 'peewee.SqliteDatabase' 
+}
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+db = Database(app)
+
+
+class Storage(db.Model):
+    data = TextField()
+
+Storage.create_table(fail_silently=True)
+
+@app.route("/store/<data>")
+def store(data):
+    return "%i" % Storage.create(data=data).id    
+
+@app.route("/dump")
+def dump():
+    datas = []
+    for data in Storage.select():
+        datas.append(data.data)
+    return '\n'.join(datas)
 
 if __name__ == "__main__":
     app.run()
